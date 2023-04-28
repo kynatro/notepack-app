0324a4a052451ef3e0181d9412a77ef3831228cd
Dave Shepard <kynatro@gmail.com>
2023-04-28 08:59:10 -0700

    Repalce Category model with Taxonomy

--- prisma/migrations/20230428155807_initial_schema/migration.sql
+++ prisma/migrations/20230428155807_initial_schema/migration.sql
@@ -1,11 +1,11 @@
 -- CreateEnum
-CREATE TYPE "CategoryType" AS ENUM ('Person', 'Project', 'Organization');
+CREATE TYPE "TaxonomyType" AS ENUM ('Person', 'Project', 'Organization');
 
 -- CreateTable
 CREATE TABLE "Alias" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "categoryId" INTEGER NOT NULL,
+    "taxonomyId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -13,23 +13,23 @@ CREATE TABLE "Alias" (
 );
 
 -- CreateTable
-CREATE TABLE "Category" (
+CREATE TABLE "Taxonomy" (
     "id" SERIAL NOT NULL,
     "active" BOOLEAN NOT NULL DEFAULT true,
     "name" TEXT NOT NULL,
     "parentId" INTEGER NOT NULL,
     "avatar" TEXT,
-    "type" "CategoryType" NOT NULL DEFAULT 'Project',
+    "type" "TaxonomyType" NOT NULL DEFAULT 'Project',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Taxonomy_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Note" (
     "id" SERIAL NOT NULL,
-    "categoryId" INTEGER NOT NULL,
+    "taxonomyId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -53,19 +53,19 @@ CREATE TABLE "Task" (
 CREATE UNIQUE INDEX "Alias_name_key" ON "Alias"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+CREATE UNIQUE INDEX "Taxonomy_name_key" ON "Taxonomy"("name");
 
 -- AddForeignKey
-ALTER TABLE "Alias" ADD CONSTRAINT "Alias_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Alias" ADD CONSTRAINT "Alias_taxonomyId_fkey" FOREIGN KEY ("taxonomyId") REFERENCES "Taxonomy"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Category" ADD CONSTRAINT "Category_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Taxonomy" ADD CONSTRAINT "Taxonomy_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Taxonomy"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Note" ADD CONSTRAINT "Note_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Note" ADD CONSTRAINT "Note_taxonomyId_fkey" FOREIGN KEY ("taxonomyId") REFERENCES "Taxonomy"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_assignedToId_fkey" FOREIGN KEY ("assignedToId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Task" ADD CONSTRAINT "Task_assignedToId_fkey" FOREIGN KEY ("assignedToId") REFERENCES "Taxonomy"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_noteId_fkey" FOREIGN KEY ("noteId") REFERENCES "Note"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
